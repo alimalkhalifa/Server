e9cb0c3f7205652d9959704b01971448d4c35e47
Paul Coene <pcoene1@twc.com>
2020-03-24 09:31:04 -0400

    Augment changes to restore messages lost to caster client and also handle Taps which were also duplicate.

--- zone/spells.cpp
+++ zone/spells.cpp
@@ -3964,9 +3964,7 @@ bool Mob::SpellOnTarget(uint16 spell_id, Mob *spelltar, bool reflect, bool use_r
 		if(IsClient())	// send to caster
 			CastToClient()->QueuePacket(action_packet);
 	}
-	// send to people in the area, ignoring caster and target
-	//live dosent send this to anybody but the caster
-	//entity_list.QueueCloseClients(spelltar, action_packet, true, 200, this, true, spelltar->IsClient() ? FILTER_PCSPELLS : FILTER_NPCSPELLS);
+	
 	message_packet = new EQApplicationPacket(OP_Damage, sizeof(CombatDamage_Struct));
 	CombatDamage_Struct *cd = (CombatDamage_Struct *)message_packet->pBuffer;
 	cd->target = action->target;
@@ -3977,7 +3975,31 @@ bool Mob::SpellOnTarget(uint16 spell_id, Mob *spelltar, bool reflect, bool use_r
 	cd->hit_heading = action->hit_heading;
 	cd->hit_pitch = action->hit_pitch;
 	cd->damage = 0;
-	if(!IsEffectInSpell(spell_id, SE_BindAffinity) && !IsDamageSpell(spell_id)){
+	
+	// attack.cpp sends out messages to nearby clients for damage and Taps
+	// Here, we only need to send out that type of a message to the caster if we are
+	// the caster or we're the owner of the pet that is the caster.
+	//
+	// This fixes the cases where other clients were getting duplicates of
+	// the messages sent by attack.cpp.
+
+	if (IsDamageSpell(spell_id) || IsLifetapSpell(spell_id)) {
+        	Client *client_to_notify=nullptr;
+
+		if (IsClient()) {
+			client_to_notify=this->CastToClient();
+		}
+		else if (IsPet() && IsPetOwnerClient()) {
+			client_to_notify=GetOwner()->CastToClient();
+		}
+
+		if (client_to_notify) {
+			client_to_notify->QueuePacket(message_packet);
+		}
+	}
+
+	if(!IsEffectInSpell(spell_id, SE_BindAffinity)
+	   && !IsDamageSpell(spell_id) && !IsLifetapSpell(spell_id)){ity) && !IsDamageSpell(spell_id)){
 		entity_list.QueueCloseClients(
 			spelltar, /* Sender */
 			message_packet, /* Packet */
